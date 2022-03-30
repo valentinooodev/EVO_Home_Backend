32e6d4f13ad6d00109d3e840b21f365327ed6a88
valentinooodev <valentinooo.dev@gmail.com>
2022-03-30 09:13:01 +0700

    feat: health check

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse
+
+
+def health(request):
+    return HttpResponse("Ok")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls', namespace='api')),
+    path('health/', health, name='health'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
